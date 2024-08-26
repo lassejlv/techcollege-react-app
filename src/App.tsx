0f9ebedbc48405a9e96a3d08e8b489093066cbf4
lassejlv <vestergaardlasse2@gmail.com>
2024-08-26 09:00:29 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import Button from "./components/Button";
-import Header from "./components/Header";
-import MyImage from "./assets/img.png";
+import { Button } from './components/Button';
+import Header from './components/Header';
+import MyImage from './assets/img.png';
+import { useState } from 'react';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(0);
+
   return (
     <>
       <Header title="This is the header" />
@@ -13,7 +17,27 @@ function App() {
 
       <Header />
 
-      <Button>This is a button</Button>
+      <Button onClick={() => setModalOpen(!modalOpen)}>{modalOpen ? 'Close' : 'Open'} Modal</Button>
+
+      <Button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Count + 1
+      </Button>
+
+      {count}
+
+      {modalOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col items-center justify-center"
+          onClick={() => setModalOpen(!modalOpen)}
+        >
+          <h1 className="text-4xl text-white">This is a modal</h1>
+          <p>This is the content of the modal, click to close it again!</p>
+        </div>
+      )}
 
       <img src={MyImage} loading="lazy" alt="My Image" />
     </>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,20 @@
-export default function Button({ children }: { children: React.ReactNode }) {
-  return (
-    <button className="bg-gray-500 hover:bg-gray-400 transition-colors ease-in-out duration-300 p-2 rounded-md text-sm ">
-      {children}
-    </button>
-  );
-}
+// export default function Button({ children }: { children: React.ReactNode }) {
+//   return (
+//     <button className="bg-gray-500 hover:bg-gray-400 transition-colors ease-in-out duration-300 p-2 rounded-md text-sm ">
+//       {children}
+//     </button>
+//   );
+// }
+
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #4d41a8;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #3e3489;
+  }
+`;
